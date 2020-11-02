ab0a16e91338ca72d8e8c75c62c2e8d94e694811
lp <2322547529@qq.com>
2020-11-02 21:37:24 +0800

    solve leetcode problems 3,4

---
+++ Code/FindMedianOfSortedArrays.java
@@ -0,0 +1,72 @@
+/**
+ * @author:linpeng
+ * @date:20201102
+ * @version:v1.0
+ */
+public class FindMedianOfSortedArrays {
+
+    /**
+     *
+     * @param nums1
+     * @param nums2
+     * @return  return the median of the two sorted arrays.
+     * The overall run time complexity should be O(log (m+n)).
+     *
+     *Example
+     * Input: nums1 = [1,2], nums2 = [3,4]
+     * Output: 2.50000
+     * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+     *
+     * Mark
+     * Runtime: 2 ms, faster than 99.70% of Java online submissions for Median of Two Sorted Arrays.
+     * Memory Usage: 40.1 MB, less than 8.87% of Java online submissions for Median of Two Sorted Arrays.
+     *
+     * reference：https://www.cnblogs.com/grandyang/p/4465932.html
+     */
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        // 正常思路：双指针遍历两个数组，找到中位数位置的值/均值  时间复杂度：O(m+n)
+        // 找到第(m+n+1)/2和(m+n+2)/2顺序的数，求均值即可
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2;
+    }
+
+    /**
+     *
+     * @param nums1
+     * @param i num1起点
+     * @param nums2
+     * @param j num2起点
+     * @param k
+     * @return 第k大小的元素
+     */
+    private static double findKth(int[] nums1, int i, int[] nums2, int j, int k) {
+        if (i >= nums1.length) {
+            return nums2[j + k - 1];
+        }
+        if (j >= nums2.length) {
+            return nums1[i + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[i], nums2[j]);
+        }
+        int midVal1 = (i + k / 2 - 1 < nums1.length) ? nums1[i + k / 2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = (j + k / 2 - 1 < nums2.length) ? nums2[j + k / 2 - 1] : Integer.MAX_VALUE;
+        if (midVal1 < midVal2) {
+            return findKth(nums1, i + k / 2, nums2, j, k - k / 2);
+        } else {
+            return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 8 9 -> 第9大小的数即 12
+        int[] nums1 = {1,5,6,9,12,32,74,99};
+        int[] nums2 = {2,4,7,9,21,66,81,120,131};
+        double result = findMedianSortedArrays(nums1, nums2);
+        System.out.println(result);
+    }
+}
---
+++ Code/LongestSubstringWithoutRepeat.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author:linpeng
+ * @date:20201101
+ * @version:v1.0
+ */
+public class LongestSubstringWithoutRepeat {
+
+    /**
+     *
+     * @param s
+     * @return the length of the longest substring without repeating characters
+     *
+     * Example
+     * Input: s = "pwwkew"
+     * Output: 3
+     * Explanation: The answer is "wke", with the length of 3.
+     * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     *
+     * Mark:
+     * Runtime: 4 ms, faster than 90.41% of Java online submissions for Longest Substring Without Repeating Characters.
+     * Memory Usage: 39.1 MB, less than 6.46% of Java online submissions for Longest Substring Without Repeating Characters.
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        if(s == null || "".equals(s)) {
+            return 0;
+        }
+
+        // i,j是两个指针,i随for循环增大，一旦遇到以前有的字符则j向后移动
+        // 如果需要知道最长字符串，则可以使用str保存，一旦res改变则重新赋值为 s[j+1, i]
+        char c;
+        int j = 0, res = 0;
+        int length = s.length();
+        Map<Character, Integer> map = new HashMap();
+        for(int i = 0; i < length; i++) {
+            c = s.charAt(i);
+            if(map.containsKey(c)) {
+                j = Math.max(j, map.get(c));
+            }
+
+            map.put(c, i + 1);
+            res = Math.max(res, i - j + 1 );
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = " abcxiwsssaa";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
