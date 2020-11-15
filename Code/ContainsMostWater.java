a85e13e98b430caef88b39252e55a8edc044f037
lp <2322547529@qq.com>
2020-11-15 22:25:03 +0800

    解决最大盛水问题和转罗马数字问题

---
+++ Code/ContainsMostWater.java
@@ -0,0 +1,40 @@
+/**
+ * @author:linpeng
+ * @date:20201115
+ * @version:v1.0
+ */
+public class ContainsMostWater {
+
+    /**
+     *
+     * @param height
+     * @return maxArea
+     *
+     * Given n non-negative integers where each represents a point at coordinate (i, ai).
+     * n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+     * Find two lines, which, together with the x-axis forms a container,
+     * such that the container contains the most water.
+     *
+     * Example
+     * Input: height = [4,3,2,1,4]
+     * Output: 16
+     *
+     * Mark
+     * Runtime: 2 ms, faster than 94.83% of Java online submissions for Container With Most Water.
+     * Memory Usage: 40.8 MB, less than 15.53% of Java online submissions for Container With Most Water.
+     */
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, maxArea = 0;
+        while(left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+
+            if(height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ Code/IntegerToRoman.java
@@ -0,0 +1,65 @@
+/**
+ * @author:linpeng
+ * @date:20201115
+ * @version:v1.0
+ */
+public class IntegerToRoman {
+
+    /**
+     *
+     * @param num
+     * @return
+     *
+     * Symbol       Value
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * Roman numerals are usually written largest to smallest from left to right.
+     * However, the numeral for four is not IIII.
+     * Instead, the number four is written as IV.
+     * Because the one is before the five we subtract it making four.
+     * The same principle applies to the number nine, which is written as IX.
+     * There are six instances where subtraction is used:
+     *
+     * Example
+     * Input: num = 1994
+     * Output: "MCMXCIV"
+     * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+     *
+     * Mark
+     * Runtime: 5 ms, faster than 56.79% of Java online submissions for Integer to Roman.
+     * Memory Usage: 38.6 MB, less than 67.75% of Java online submissions for Integer to Roman.
+     */
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbtemp = new StringBuilder();
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        for (int i = 0; i < values.length; i++) {
+            int r = num / values[i];
+            sb.append(getRepeatString(sbtemp, symbols[i], r));
+            num -= r * values[i];
+        }
+
+        return sb.toString();
+    }
+
+    private static String getRepeatString(StringBuilder sb, String s, int r) {
+        sb.setLength(0);
+        while(r > 0) {
+            sb.append(s);
+            r--;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int num = 1994;
+        System.out.println(intToRoman(num));
+    }
+}
