a648bb6a38c5b420e9c213617fc94b69a2eb4762
lp <2322547529@qq.com>
2020-11-17 21:12:19 +0800

    解决【三数最近和】【手机数字转字符组合】问题

---
+++ Code/LetterCombination.java
@@ -0,0 +1,70 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+/**
+ * @author:linpeng
+ * @date:20201117
+ * @version:v1.0
+ */
+public class LetterCombination {
+
+    /**
+     *
+     * @param digits
+     * @return all possible letter combinations that the number could represent
+     *
+     * Given a string containing digits from 2-9 inclusive
+     * A mapping of digit to letters (just like on the telephone buttons) is given below.
+     * Note that 1 does not map to any letters.
+     *
+     * Example
+     * input："23"
+     * output：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+     *
+     * Mark
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Letter Combinations of a Phone Number.
+     * Memory Usage: 37.6 MB, less than 88.77% of Java online submissions for Letter Combinations of a Phone Number.
+     */
+    public static List<String> letterCombinations(String digits) {
+        if(digits == null || "".equals(digits)) {
+            return new ArrayList<>();
+        }
+
+        char[] arr = digits.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        List<String> res = new ArrayList<>();
+        HashMap<Character, String> map = new HashMap<>(8);
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+
+        getCombination(0, arr, map, res, sb);
+        return res;
+    }
+
+    private static void getCombination(int pos, char[] arr, Map<Character, String> map, List<String> res, StringBuilder sb) {
+        if(pos == arr.length) {
+            if(sb.length() != 0) {
+                res.add(sb.toString());
+            }
+            return;
+        }
+
+        for(char c : map.get(arr[pos]).toCharArray()) {
+            sb.append(c);
+            getCombination(pos + 1, arr, map, res, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "2423";
+        System.out.println(letterCombinations(s));
+    }
+}
---
+++ Code/ThreeSumClosest.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+/**
+ * @author:linpeng
+ * @date:20201117
+ * @version:v1.0
+ */
+public class ThreeSumClosest {
+
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return the sum of three integers in nums such that the sum is closest to target
+     *
+     * Example
+     * Input: nums = [-1,2,1,-4], target = 1
+     * Output: 2
+     * Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+     *
+     * Runtime: 4 ms Your runtime beats 85.04 % of java submissions.
+     * Memory Usage: 39 MB Your memory usage beats 31.42 % of java submissions.
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        int diff = Integer.MAX_VALUE;
+        int length = nums.length;
+
+        // 和threeSum一类问题，排序后双指针 O(n^2)
+        Arrays.sort(nums);
+        for(int i = 0; i < length && diff != 0; i++) {
+            int leftpos = i + 1;
+            int rightpos = length - 1;
+            while(leftpos < rightpos) {
+                int sum = nums[i] + nums[leftpos] + nums[rightpos];
+                if(Math.abs(sum - target) < Math.abs(diff)) {
+                    diff = target - sum;
+                }
+                if(sum > target) {
+                    rightpos --;
+                } else {
+                    leftpos ++;
+                }
+            }
+        }
+        return target - diff;
+    }
+}
